7db908c6bce3d107de6d3faa419bc1bc9577b9a6
jeng-yuantsai <tsaie79@gmail.com>
2024-04-09 05:09:45 +0000

    Update run_model.sh to redirect launch output to log file

--- run/run_model.sh
+++ run/run_model.sh
@@ -4,4 +4,4 @@
 # run ros2
 cd ~/digitaltwin_ws
 . install/setup.bash
-ros2 launch digitaltwin digitaltwin_launch_gui.py
+ros2 launch digitaltwin digitaltwin_launch.py > /workspaces/UAV-Digital-Twin/log.txt 2>&1
